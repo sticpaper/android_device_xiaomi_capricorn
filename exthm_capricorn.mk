972a424b0f43dd56f4272631baa981eba54a8c62
stic-server-open <1138705738@qq.com>
2021-02-13 07:36:22 +0800

    capricorn: exTHmUI-ify
    Hello exTHmUI

--- exthm_capricorn.mk
+++ exthm_capricorn.mk
@@ -22,10 +22,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/capricorn/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/exthm/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_capricorn
+PRODUCT_NAME := exthm_capricorn
 PRODUCT_DEVICE := capricorn
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 5s
